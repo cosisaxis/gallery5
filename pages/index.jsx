1c024fe1bad6a66d5b9addaaa2a429f10077158e
cosisaxis <kishanelewis2@gmail.com>
2022-11-21 17:29:07 -0500

    setting-data

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,15 +4,22 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
 export default function Home() {
-  const [wallet, setWallet] = useState("")
+  const [wallet, setWallet] = useState("");
+  const [collection, setCollection] =  useState("");
+
+  const nftGrabber = () => {
+
+  }
 
   return (
     <div className='flex min-h-screen flex-col items-center justify-center '>
       <div>
         <input onChange={(e) => {setWallet(e.target.value)}} value={wallet} type={"text"} placeholder="Your wallet address goes here"></input>
-        <input type={"text"} placeholder="Your collection goes here"></input>
+        <input onChange={(e) => {setCollection(e.target.value)}} type={"text"} placeholder="Your collection goes here"></input>
         <label><input type={"checkbox"}></input></label>
-        <button>Search</button>
+        <button onClick={() => {
+           nftGrabber
+        }}>Search</button>
       </div>
     </div>
   )
